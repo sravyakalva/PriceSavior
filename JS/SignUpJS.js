6dd62631214e5a513802eea1bc9356e4fd43701d
sravyakalva <kalvasravya@gmail.com>
2016-09-10 17:34:32 -0400

    Add files via upload

---
+++ JS/SignUpJS.js
@@ -0,0 +1,27 @@
+/**
+ * Created by sravyakalva on 9/10/16.
+ */
+$(document).ready(function (){
+    validate();
+    $('input').on('keyup', validate);
+});
+
+function validate(){
+
+    var inputsWithValues = 0;
+
+    // get all input fields except for type='submit'
+    var myInputs = $("input:not([type='submit'])");
+
+    myInputs.each(function() {
+        // if it has a value, increment the counter
+        if ($(this).val()) {
+            inputsWithValues += 1;
+        }
+    });
+    if (inputsWithValues == myInputs.length) {
+        $("input[type=submit]").prop("disabled", false);
+    } else {
+        $("input[type=submit]").prop("disabled", true);
+    }
+}
