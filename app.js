fffae4413c880b01cee89ff390ff0b45a80820d1
Michael Morris <vinh.morris@gmail.com>
2016-10-26 17:16:07 -0400

    Added nodejs

---
+++ app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kikovinh on 10/26/2016.
+ */
+var	express	= require('express');
+var	app	= express();
+var	port	=	process.env.port	||	3000;
+app.get('/', function (req,	res) {
+    res.send('Hello	World!');
+});
+app.listen(port, function () {
+    console.log('Example	app	listening	on	port'	+	port);
+});
