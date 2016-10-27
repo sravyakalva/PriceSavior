f4441b054c88cd5eb7abf1f44786fad79fae703a
Michael Morris <vinh.morris@gmail.com>
2016-10-27 16:48:28 -0400

    Fixed firebase and nodejs

--- index.js
+++ index.js
@@ -3,13 +3,29 @@ var express = require('express');
 var app = express();
 
 firebase.initializeApp({
-  serviceAccount: "./PriceSavior-6bc819483d90.json",
+  serviceAccount: "PriceSavior-6bc819483d90.json",
   databaseURL: "https://pricesavior-23da4.firebaseio.com"
 });
+var accountsRef = firebase.database().ref("User Accounts");
 
 var port = process.env.PORT || 3000;
 
 var counter = 0;
+
+app.post('/createAccount', function (req, res) {
+  console.log("New User");
+  //Sets the node to the email, each node is a child of User Accounts
+  var node = accountsRef.child(account);
+  node.set({
+    "Username": req.body.accountNew[0],
+    "Password": req.body.accountNew[1],
+    "Zipcode": req.body.accountNew[2],
+    "Email": req.body.accountNew[3]
+  });
+});
+
+
+
 app.get('/', function (req, res) {
   res.send('Hello World has been said ' + counter + ' times!');
   counter++;
