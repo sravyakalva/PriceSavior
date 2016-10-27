268b227e464c4e89b805179e9d0a39576373e923
Michael Morris <vinh.morris@gmail.com>
2016-10-27 16:50:45 -0400

    Fixed firebase and nodejs

--- public/JS/HomePage.js
+++ public/JS/HomePage.js
@@ -16,23 +16,17 @@ $(document).ready(function ()
         $('#Registration').submit(function(ev){
 
             ev.preventDefault(); // to stop the form from submitting
-            var username = $("input[name='username']").val();
-            var password = $("input[name='password']").val();
-            var zipcode = $("input[name='zipcode']").val();
+            var accountInput = [];
+            accountInput.push($("input[name='username']").val());
+            accountInput.push($("input[name='password']").val());
+            accountInput.push($("input[name='zipcode']").val());
             var email = $("input[name='email']").val();
 
             //Replaces "." since Firebase does not accept periods
-            var account = email.replace(/\./g,'|');
+            email = email.replace(/\./g,'|');
 
-
-            //Sets the node to the email, each node is a child of User Accounts
-            var node = accountsRef.child(account);
-            node.set({
-                "Username": username,
-                "Password": password,
-                "Zipcode":zipcode,
-                "Email": email
-            });
+            accountInput.push(email);
+            $.post("/createAccount",{accountNew: accountInput});
 
             //Closes the popup after hitting "submit"
             $('#signUpForm').dialog('close');
