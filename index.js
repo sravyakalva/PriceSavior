30a30546bd513ebfd5576bc3f35ae32195a13d22
Michael Morris <vinh.morris@gmail.com>
2016-11-02 14:07:41 -0400

    Fixed nodejs firebase

---
+++ index.js
@@ -0,0 +1,58 @@
+/**
+ * Created by kikovinh on 10/26/2016.
+ */
+var firebase = require("firebase");
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+    extended: true
+}));
+
+
+firebase.initializeApp({
+    serviceAccount:"./pricesavior-node-client-service-account.json",
+    databaseURL:"https://pricesavior-23da4.firebaseio.com"
+});
+
+var ref = firebase.database().ref('Users');
+ref.once('value').then(function(snap){
+    console.log('snap.val()', snap.val());
+});
+
+
+var	port	=	process.env.port	||	3000;
+
+
+app.post('/HTML/Home.html', function (req, res) {
+
+    var email = req.body.email;
+    email = email.replace(/\./g, '|');
+
+    //Sets the node to the email, each node is a child of User Accounts
+    ref.child(email).set({
+            Username: req.body.username,
+            Password: req.body.confirmPassword,
+            Zipcode: req.body.zipcode
+    },function () {
+        res.sendFile('public/HTML/Home.html', { root : __dirname});
+    }).catch(function(){
+        res.status(403);
+        res.send();
+    });
+
+    console.log("You have created a new user");
+});
+
+
+app.use(express.static('public'));
+
+app.get('/', function (req,	res) {
+    res.send('Hello	World!');
+});
+
+
+app.listen(port, function () {
+    console.log('Example	app	listening	on	port'	+	port);
+});
--- public/JS/HomePage.js
+++ public/JS/HomePage.js
@@ -3,7 +3,7 @@
  */
 
 //Sets the root of the accounts
-var accountsRef = firebase.database().ref("User Accounts");
+var accountsRef = firebase.database().ref("Users");
 
 $(document).ready(function () 
 {
@@ -11,29 +11,8 @@ $(document).ready(function ()
     $('#signUpForm').dialog({autoOpen:false})
     $('#signUpLink').click(function(){
        $('#signUpForm').dialog('open');
-       // validate();
-       // $('input').on('keyup', validate);
+
         $('#Registration').submit(function(ev){
-
-            ev.preventDefault(); // to stop the form from submitting
-            var username = $("input[name='username']").val();
-            var password = $("input[name='password']").val();
-            var zipcode = $("input[name='zipcode']").val();
-            var email = $("input[name='email']").val();
-
-            //Replaces "." since Firebase does not accept periods
-            var account = email.replace(/\./g,'|');
-
-
-            //Sets the node to the email, each node is a child of User Accounts
-            var node = accountsRef.child(account);
-            node.set({
-                "Username": username,
-                "Password": password,
-                "Zipcode":zipcode,
-                "Email": email
-            });
-
             //Closes the popup after hitting "submit"
             $('#signUpForm').dialog('close');
         });
