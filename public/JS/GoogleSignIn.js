6e5151cfc7e4718bea4b1e7689d12419891ca4bf
Michael Morris <vinh.morris@gmail.com>
2016-11-03 00:27:31 -0400

    Fixed google sign-in page

--- public/JS/GoogleSignIn.js
+++ public/JS/GoogleSignIn.js
@@ -2,13 +2,22 @@
  * Created by kikovinh on 11/2/2016.
  */
 var provider = new firebase.auth.GoogleAuthProvider();
-
+var user;
+$(document).ready(function(){
+   $("#Welcome").hide();
+});
 function signIn(){
     firebase.auth().signInWithPopup(provider).then(function(result) {
         // This gives you a Google Access Token. You can use it to access the Google API.
         var token = result.credential.accessToken;
         // The signed-in user info.
-        var user = result.user;
+        user = result.user;
+        $("#login").hide();
+        $("#Welcome").show();
+        $("#welcometext").html("Hello, "+ user.displayName);
+        $("#welcometext").css({"text-align":"center"});
+        $("#clickOnHome").html(" <a class='btn btn-lg btn-primary btn-block' style='background:orange' href='Home.html'>Go To Home</a>");
+
         // ...
     }).catch(function(error) {
         // Handle Errors here.
